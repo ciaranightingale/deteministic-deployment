81330e81894d0ea2d599a21739c7be9143022c56
ciaranightingale <ciara.nightingale@sky.com>
2023-09-07 22:51:23 +0100

    init

---
+++ index.ts
@@ -0,0 +1,74 @@
+import { config } from "dotenv";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import {
+  ArbitrumGoerli,
+  BaseGoerli,
+  CeloAlfajoresTestnet,
+  Goerli,
+  LineaTestnet,
+  Mumbai,
+  OptimismGoerli,
+  Sepolia,
+} from "@thirdweb-dev/chains";
+
+config();
+
+// chains to deploy to
+const chainsToDeployTo = [
+  Goerli,
+  Mumbai,
+  Sepolia,
+  BaseGoerli,
+  OptimismGoerli,
+  ArbitrumGoerli,
+  LineaTestnet,
+  CeloAlfajoresTestnet,
+];
+// the name of the published contract to deploy ex: AccountFactory for the contract https://thirdweb.com/thirdweb.eth/AccountFactory
+const publishedContractToDeploy = "Token Bound Account";
+// The full list of constructor arguments for the published contract (for AccountFactory we just need the Entrypoint address)
+const publishedContractConstructorArguments = [
+  "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+  "0x02101dfB77FDE026414827Fdc604ddAF224F0921",
+];
+
+const main = async () => {
+  if (!process.env.WALLET_PRIVATE_KEY) {
+    throw new Error("No private key found");
+  }
+
+  try {
+    for (const chain of chainsToDeployTo) {
+      const sdk = ThirdwebSDK.fromPrivateKey(
+        process.env.WALLET_PRIVATE_KEY,
+        chain,
+        {
+          secretKey: process.env.THIRDWEB_SECRET_KEY,
+        }
+      );
+      // shows how to predict the address of any published contract
+      const predictedAddress = await sdk.deployer.predictAddressDeterministic(
+        publishedContractToDeploy,
+        publishedContractConstructorArguments
+      );
+      console.log("deploying on", chain.slug, "at address:", predictedAddress);
+
+      // shows how to deploy a published contract at a deterministic address
+      const deployedAddress =
+        await sdk.deployer.deployPublishedContractDeterministic(
+          publishedContractToDeploy,
+          publishedContractConstructorArguments
+        );
+      console.log(
+        "--> succesfully deployed on",
+        chain.slug,
+        "at address:",
+        deployedAddress
+      );
+    }
+  } catch (e) {
+    console.error("Something went wrong: ", e);
+  }
+};
+
+main();
